9a12455a366d4ef26c96fdae84567e8ed63a11c3
JackqqWang <yingsuhua@zju.edu.cn>
2024-05-06 11:26:09 -0400

    add

--- 0505.sh
+++ 0505.sh
@@ -5,10 +5,13 @@
 
 
 # centralized training on syn data
-CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset domainnet --mode train_syn --centralized_epochs 20 --centralized_lr 1e-4  | tee results/central_domainnet_train_syn.txt
-CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset imageclef_da --mode train_syn --centralized_epochs 20 --centralized_lr 1e-4  | tee results/central_imageclef_da_train_syn.txt
+# CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset domainnet --mode train_syn --centralized_epochs 20 --centralized_lr 1e-4  | tee results/central_domainnet_train_syn.txt
+# CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset imageclef_da --mode train_syn --centralized_epochs 20 --centralized_lr 1e-4  | tee results/central_imageclef_da_train_syn.txt
 
 # # centralized training on syn + real data
 # CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset officehome --mode train_syn_real --centralized_epochs 20 --centralized_lr 1e-4 | tee results/central_officehome_train_syn_real.txt
+CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset domainnet --mode train_syn_real --centralized_epochs 20 --centralized_lr 1e-4 | tee results/central_domainnet_train_syn_real.txt
+CUDA_VISIBLE_DEVICES=0 python Centralized_training.py --dataset imageclef_da --mode train_syn_real --centralized_epochs 20 --centralized_lr 1e-4 | tee results/central_imageclef_da_train_syn_real.txt
+
 
 # test git push
