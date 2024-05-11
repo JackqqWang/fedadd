8794a24e09dc29a6b9ac45cbf5f1aa59f5084af5
JackqqWang <yingsuhua@zju.edu.cn>
2024-05-11 02:28:26 -0400

    add ssh new ssh

--- 0510_fed_officehome.sh
+++ 0510_fed_officehome.sh
@@ -1,2 +1,2 @@
 # officehome
-CUDA_VISIBLE_DEVICES=1 python FedAdapter.py --dataset officehome --interact_epoch 10 --lr 1e-3 --num_clients 3 --out_domain 1 | tee results/0510_fed_officehome.txt
+CUDA_VISIBLE_DEVICES=1 python FedAdapter.py --dataset officehome --interact_epoch 10 --lr 1e-3 --lr_local 1e-4 --num_clients 3 --out_domain 1 | tee results/0510_fed_officehome.txt
