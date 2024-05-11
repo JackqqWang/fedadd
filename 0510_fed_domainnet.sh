f7c2e6229e8d40061ca13106a2a0a0d7ed62e87c
JackqqWang <yingsuhua@zju.edu.cn>
2024-05-11 00:34:42 -0400

    add ssh domainnetssh

--- 0510_fed_domainnet.sh
+++ 0510_fed_domainnet.sh
@@ -1,2 +1,2 @@
 # domainnet
-CUDA_VISIBLE_DEVICES=2 python FedAdapter.py --dataset domainnet --num_clients 3 --out_domain 1,3,5 --server_syn_size 20000 | tee results/0510_fed_domainnet.txt
+CUDA_VISIBLE_DEVICES=2 python FedAdapter.py --dataset domainnet --interact_epoch 10 --lr 1e-3 --num_clients 3 --out_domain 1,3,5 --server_syn_size 20000 | tee results/0510_fed_domainnet.txt
